568d88a0370afe15010ce01270a33ddc25707de6
bhaumik.patel <bhaumik.patel@visicase.com>
2020-04-08 19:53:17 +1000

    Updates

---
+++ precache-manifest.e4ddeb9418d340a1fbac0fb93eada79f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada02ae4b5e3863d9789505ac9334434",
+    "url": "/my-app1/index.html"
+  },
+  {
+    "revision": "684dfca20a150d9283fb",
+    "url": "/my-app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9827ea961725702a6ef3",
+    "url": "/my-app1/static/js/2.3285bd9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app1/static/js/2.3285bd9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684dfca20a150d9283fb",
+    "url": "/my-app1/static/js/main.ed9f8d07.chunk.js"
+  },
+  {
+    "revision": "a01dfe12233adcf01561",
+    "url": "/my-app1/static/js/runtime-main.8b2996d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
